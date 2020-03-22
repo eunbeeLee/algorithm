3692bc65a8b6953a5c374dd5cf5bce9488363a06
eunbee <eunbee1224@gmail.com>
2020-03-23 00:00:23 +0900

    <feature>: solve jumping on the clouds(hakerrank)

---
+++ src/main/java/hakerrank/JumpingOnTheClouds.java
@@ -0,0 +1,26 @@
+package hakerrank;
+
+import java.util.Stack;
+
+public class JumpingOnTheClouds {
+
+    static int jumpingOnClouds(int[] c) {
+
+        Stack cloudStack = new Stack();
+        int jump = 0;
+
+        for (int cloud : c) {
+            if (cloud == 1){
+                jump++;
+                cloudStack.clear();
+            }else {
+                cloudStack.add(cloud);
+            }
+            if (cloudStack.size() >= 2){
+                jump++;
+                cloudStack.clear();
+            }
+        }
+        return jump;
+    }
+}
---
+++ src/test/java/hakerrank/JumpingOnTheCloudsTest.java
@@ -0,0 +1,23 @@
+package hakerrank;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.Assert.*;
+
+public class JumpingOnTheCloudsTest {
+
+    @Test
+    public void jumpingOnClouds(){
+        int[] arr = {0, 0, 0, 0, 1, 0};
+
+        int result = JumpingOnTheClouds.jumpingOnClouds(arr);
+        assertEquals(3, result);
+
+        int[] arr2 = {0, 0, 1, 0, 0, 1, 0};
+        int result2 = JumpingOnTheClouds.jumpingOnClouds(arr2);
+        assertEquals(4, result2);
+
+    }
+
+}
