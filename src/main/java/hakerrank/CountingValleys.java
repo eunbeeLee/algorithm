6dcc363b6f0b828d726da0461b1d3a5e65a721f5
eunbeeLee <eunbee@nomadconnection.com>
2020-03-24 18:27:56 +0900

    <feature>: 'counting valleys' (hekerrank)

---
+++ src/main/java/hakerrank/CountingValleys.java
@@ -0,0 +1,28 @@
+package hakerrank;
+
+/**
+ * [Function Description]
+ * Complete the countingValleys function in the editor below. It must return an integer that denotes the number of valleys Gary traversed.
+ *
+ * countingValleys has the following parameter(s):
+ *
+ * n: the number of steps Gary takes
+ * s: a string describing his path
+ *
+ * [Input Format]
+ * The first line contains an integer , the number of steps in Gary's hike.
+ * The second line contains a single string , of  characters that describe his path.
+ *
+ * [Output Format]
+ * Print a single integer that denotes the number of valleys Gary walked through during his hike.
+ */
+public class CountingValleys {
+
+    // Complete the countingValleys function below.
+    static int countingValleys(int n, String s) {
+        return 0;
+
+    }
+
+
+}
---
+++ src/test/java/hakerrank/CountingValleysTest.java
@@ -0,0 +1,17 @@
+package hakerrank;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CountingValleysTest {
+
+    @Test
+    public void countingValleys() {
+        int n = 8;
+        String s = "UDDDUDUU";
+        int result = CountingValleys.countingValleys(n, s);
+
+        assertEquals(1, result);
+    }
+}
