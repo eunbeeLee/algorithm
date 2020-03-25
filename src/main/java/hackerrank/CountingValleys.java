4567d667842a415b24c903ecf0e69ac63ca58c96
eunbeeLee <eunbee@nomadconnection.com>
2020-03-25 20:58:01 +0900

    <feature>: solving counting valleys

--- src/main/java/hackerrank/CountingValleys.java
+++ src/main/java/hackerrank/CountingValleys.java
@@ -1,5 +1,10 @@
 package hackerrank;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.Stack;
+
 /**
  * [Function Description]
  * Complete the countingValleys function in the editor below. It must return an integer that denotes the number of valleys Gary traversed.
@@ -20,8 +25,33 @@ public class CountingValleys {
 
     // Complete the countingValleys function below.
     static int countingValleys(int n, String s) {
-        return 0;
+        int valley = 0;
+        int seaLevel = 0;
 
+        Stack<Character> pathStack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++){
+            char path = s.charAt(i);
+
+            if (i > 0 && seaLevel == 0){
+                int halfStack = pathStack.size() % 2;
+                int pathCheck = 0;
+
+                for (int j = 0 ; j < halfStack; j++){
+                    if (pathStack.get(j) == 'D'){
+                        break;
+                    }
+                    pathCheck++;
+                }
+                if (pathCheck == halfStack){
+                    valley++;
+                }
+            }
+            seaLevel += path == 'D' ? -1 : 1;
+            pathStack.add(path);
+
+        }
+        return valley;
     }
 
 
