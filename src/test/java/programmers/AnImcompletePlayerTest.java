11cd2e4d38bcdbac1f263e064efae1d4c5cd6d78
eunbee <eunbee1224@gmail.com>
2020-05-27 23:49:19 +0900

    <feature>: AnIncompletePlayer-add test cases

--- src/test/java/programmers/AnImcompletePlayerTest.java
+++ src/test/java/programmers/AnImcompletePlayerTest.java
@@ -2,6 +2,10 @@ package programmers;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class AnImcompletePlayerTest {
@@ -13,5 +17,15 @@ public class AnImcompletePlayerTest {
         String[] completionList = {"eden", "kiki"};
         String result = anImcompletePlayer.solution(participantList, completionList);
         assertEquals("leo", result);
+
+        participantList = new String[]{"marina", "josipa", "nikola", "vinko", "filipa"};
+        completionList =new String[]{"josipa", "filipa", "marina", "nikola"};
+        result = anImcompletePlayer.solution(participantList, completionList);
+        assertEquals("vinko", result);
+
+        participantList = new String[]{"mislav", "stanko", "mislav", "ana"};
+        completionList =new String[]{"stanko", "ana", "mislav"};
+        result = anImcompletePlayer.solution(participantList, completionList);
+        assertEquals("mislav", result);
     }
 }
