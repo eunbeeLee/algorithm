5d1b2facd1fe4de14859415818a1ef84304729e6
eunbee <eunbee1224@gmail.com>
2020-05-28 23:56:52 +0900

    <modify>: solving -AnIncompletePlayer

--- src/main/java/programmers/AnImcompletePlayer.java
+++ src/main/java/programmers/AnImcompletePlayer.java
@@ -1,26 +1,27 @@
 package programmers;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
  * https://programmers.co.kr/learn/courses/30/lessons/42576
  */
 class AnImcompletePlayer {
-    public String solution(String[] participantList, String[] completionList) {
-        Map<String, String> playerMap = new HashMap<>();
+    public String solution(String[] participantArr, String[] completionArr) {
+        String result = "";
+        Map<String, String> map = new HashMap<>();
+        List<String> participantList = Arrays.asList(participantArr);
 
-        for (String participant : participantList) {
-            playerMap.put(participant, "");
-        }
+        for (int i = 0 ; i< participantArr.length; i++){
+            for(String completion: completionArr){
+                if (participantArr[i].equals(completion)){
 
-        for (String completion : completionList){
-            if (playerMap.containsKey(completion)){
-                playerMap.remove(completion);
+                    break;
+                }
             }
         }
-
-
-        return playerMap.keySet().iterator().next();
+        return result;
     }
 }
