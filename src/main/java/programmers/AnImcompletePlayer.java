21126b3a8c33ee755a48dc62fcc389fd2c67acf4
eunbee <eunbee1224@gmail.com>
2020-05-30 23:57:52 +0900

    <modify>: solved -AnIncompletePlayer

--- src/main/java/programmers/AnImcompletePlayer.java
+++ src/main/java/programmers/AnImcompletePlayer.java
@@ -1,28 +1,24 @@
 package programmers;
 
-import java.util.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * https://programmers.co.kr/learn/courses/30/lessons/42576
  */
 class AnImcompletePlayer {
     public String solution(String[] participantArr, String[] completionArr) {
-        String result = "";
-        Map<String, String> map = new HashMap<>();
         List<String> participantList = new ArrayList<>();
         participantList.addAll(Arrays.asList(participantArr));
-        List<String> completionList = new ArrayList<>();
-        completionList.addAll(Arrays.asList(completionArr));
 
-        for (String participant : participantArr){
-            for(String completion: completionList){
-                if (participant.equals(completion)){
-                    participantList.remove(participantList.indexOf(participant));
-                    completionList.remove(participantList.indexOf(participant));
-                    break;
-                }
+        for (String completion: completionArr){
+            if (participantList.contains(completion)){
+                participantList.remove(participantList.indexOf(completion));
             }
         }
+
         return participantList.get(0);
     }
 }
